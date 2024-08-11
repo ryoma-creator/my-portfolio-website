687dc34c06b972d4853fbdf5d02b3d9681dc63bd
Ryoma/Cross <ryoma111326@gmail.com>
2024-08-11 18:57:04 +0800

    Add custom colors and JetBrains Mono font to Tailwind config
    - Added text color: #ffffff (white)
    - Added background color: #1c1c22 (dark gray/almost black)
    - Added accent color: #00ff99 (green)
    - Applied custom font and colors in base layer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,10 +7,36 @@ const Navbar = () => {
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
         <div>
-          <p>Ryoma<span className='text-green ' >.</span></p>
             <img src={Logo} alt="Logo Image" style={{width: '50px'}}/>
         </div>
+        {/* menu */}
+        <div>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>SKills</li>
+                <li>Work</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+
+        {/* Hamburger */}
+        <div>
+              <FaBars className='hidden'/>
+        </div>
+
+        {/* Mobile menu */}
+            <ul className='hidden'>
+                <li>Home</li>
+                <li>About</li>
+                <li>SKills</li>
+                <li>Work</li>
+                <li>Contact</li>
+            </ul>        
+
     </div>
+
+
   )
 }
 
