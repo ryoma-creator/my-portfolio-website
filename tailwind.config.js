a9d32c6123cd458e3a4e54411d88a51a0e7a2f95
Ryoma/Cross <ryoma111326@gmail.com>
2024-08-11 18:38:48 +0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        black:"#1c1c22",  // bg color 背景色
+        green:"#00ff99",   // accent color（緑）
+      },
+      fontFamily: {
+        jetbrains: ['JetBrains Mono', 'monospace'], // 'jetbrains' という名前でフォントを定義
+      },
+    },
+  },
+  plugins: [],
+}
+
