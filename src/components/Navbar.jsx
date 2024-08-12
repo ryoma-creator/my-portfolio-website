2e0e8d6a804eec25d20cf888416fa7d1dc2ebc11
Ryoma/Cross <ryoma111326@gmail.com>
2024-08-12 12:44:59 +0800

    Add social icons to Navbar with hover effect - Implemented social icons using map function. - Added hover effect to slide icons into view. - Updated Navbar component with new social media links.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,22 @@
 import React, {useState} from 'react'
-import {FaBars, FaTimes} from 'react-icons/fa'
+import {FaBars, FaTimes, FaGithub, FaLinkedin, FaFacebook} from 'react-icons/fa'
+import {HiOutlineMail} from 'react-icons/hi'
+import {BsFillPersonLinesFill} from 'react-icons/bs'
 import Logo from '../assets/logo1.png'
 
+const socialLinks = [
+  { name: 'Linkedin', icon: FaLinkedin, color: 'bg-blue-600', url: '/' },
+  { name: 'Github', icon: FaGithub, color: 'bg-[#333333]', url: '/' },
+  { name: 'Email', icon: HiOutlineMail, color: 'bg-[#6fc2b0]', url: '/' },
+  { name: 'Resume', icon: BsFillPersonLinesFill, color: 'bg-[#565f69]', url: '/' },
+];
 
 const Navbar = () => {
     const [nav, setNav] = useState(false)
     const handleClick = () => setNav(!nav)
-
+    
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 text-gray-300'>
         <div className='w-12 text-5xl font-signature'>
             <h1>RT<span className='text-green'>.</span></h1>
             {/* <img src={Logo} alt="Logo Image" style={{width: '50px'}}/> */}
@@ -37,11 +45,23 @@ const Navbar = () => {
         </ul>        
 
         {/* Social Icons */}
-        <div className='hidden'></div>
-    </div>
+        <div className='flex fixed flex-col top-[35%] left-0'>
+            <ul>
+              {socialLinks.map(({ name, icon: Icon, color, url })=>(
+                  <li key={name} className={`w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 ${color}`}>
+                  <a className='flex justify-between items-center w-full text-gray-300'
+                  href={url}>
+                    {name} <Icon size={30}/>
+                  </a>
+                </li>
+              ))}
+
+            </ul>
+        </div>
+      </div>
 
 
-  )
-}
+  );
+};
 
 export default Navbar;
